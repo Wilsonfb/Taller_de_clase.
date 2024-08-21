ab322b7da681941d9d5030947c9dfedd64ba2e92
Paula Arias <paularias.pq@gmail.com>
2024-08-21 13:01:41 -0500

    Taller

---
+++ Taller.py
@@ -0,0 +1,7 @@
+# 1. Crear un repositorio.
+# 2. Crear 3 ramas.
+# Elabore un algoripmo que muestre un menu de obciones, las obciones son las siguientes, subir en la primera rama:
+# A. Jugar.
+# B. Calcular.
+# C. Salir.
+# Elabore un algoridmo que calcule el promedio de las edades de la ficha, subir a la segunda rama.
